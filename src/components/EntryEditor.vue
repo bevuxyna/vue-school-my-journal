2ed956d6302c4b33abc829498016f83a71e7da83
vmelnikova <vmelnikova@phoenixit.ru>
2023-12-13 13:28:53 +0300

    typing computed props

--- src/components/EntryEditor.vue
+++ src/components/EntryEditor.vue
@@ -1,18 +1,19 @@
 <script lang="ts" setup>
 import EmojiField from "@/components/EmojiField.vue";
 import ArrowCircleRight from "@/assets/icons/arrow-circle-right.svg";
-import {ref} from "vue";
+import {computed, ref} from "vue";
 import type {Emoji} from "@/types/Emoji";
 
 const text = ref('');
 const emoji = ref<Emoji | null>(null);
+const charCount = computed(() => text.value.length);
 </script>
 <template>
   <form class="entry-form" @submit.prevent>
     <textarea v-model="text" placeholder="New Journal Entry for vmelnikova_io"></textarea>
     <EmojiField v-model="emoji" />
     <div class="entry-form-footer">
-      <span>0 / 280</span>
+      <span>{{ charCount }} / 280</span>
       <button>Remember <ArrowCircleRight width="20" /></button>
     </div>
   </form>
